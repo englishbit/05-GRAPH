26aecde5bc21cc00d3447492a9dfbd9dbc925ba0
SK <englishenglishenglish@proton.me>
2025-07-09 16:26:13 +0000

    Update 04-Adjacency matrix implementation.c

--- 04-Adjacency matrix implementation.c
+++ 04-Adjacency matrix implementation.c
@@ -5,9 +5,10 @@
 int main(){
 
    int node,edge;
+
+   //logic for taking input
    scanf("%d %d",&node,&edge);
    int adj_mtrx[node][node];
-
 
    for(int i=0;i<node;i++){
           for(int j=0;j<node;j++){
@@ -17,13 +18,16 @@ int main(){
    
 
    int strt,end;
+
+   //logic for developing matrix
    for(int i=0;i<edge;i++){
         scanf("%d %d",&strt,&end);
         adj_mtrx[strt][end]=1;
         adj_mtrx[end][strt]=1;
    }
 
-   
+
+   //logic for output
    for(int i=0;i<node;i++){
         for(int j=0;j<node;j++){
             printf("%d ",adj_mtrx[i][j]);

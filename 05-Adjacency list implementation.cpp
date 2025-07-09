a1c4d29634130bbbd7d6078de2cccbb8c884b30d
SK <englishenglishenglish@proton.me>
2025-07-09 16:19:41 +0000

    Update 05-Adjacency list implementation.cpp

--- 05-Adjacency list implementation.cpp
+++ 05-Adjacency list implementation.cpp
@@ -2,11 +2,16 @@
 #include<vector>
 using namespace std;
 int main(){
+            
             int node,edge;
+
+            //logic for taking input
             cin>>node>>edge;
             vector <int> vec[node];
             
             int n1,n2;
+
+            //logic for taking input
             for(int i=0;i<edge;i++){
                 cin>>n1>>n2;
                 vec[n1].push_back(n2);
@@ -14,21 +19,14 @@ int main(){
             }
             
             cout<<endl<<endl;
-            
+
+            //logic for output
             for(int i=0;i<node;i++){
                 for(int j=0;j<vec[i].size();j++){
                     cout<<vec[i][j]<<" ";
                 }
                 cout<<endl;
             }
-
-
-
-
-
-
-
-
 
 
 
